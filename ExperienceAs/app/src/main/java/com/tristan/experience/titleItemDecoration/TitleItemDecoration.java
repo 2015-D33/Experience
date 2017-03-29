eb8e154ea13749f8387fcb45f26aa55230862808
hanguodong <hanguodong@zhihu.com>
2017-03-29 14:40:46 +0800

    no message

---
+++ ExperienceAs/app/src/main/java/com/tristan/experience/titleItemDecoration/TitleItemDecoration.java
@@ -0,0 +1,30 @@
+package com.tristan.experience.titleItemDecoration;
+
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * @author hanguodong @ Zhihu Inc.
+ * @since 02-14-2017
+ */
+
+public class TitleItemDecoration extends RecyclerView.ItemDecoration {
+
+	@Override
+	public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+		super.onDraw(c, parent, state);
+	}
+
+	@Override
+	public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+		super.onDrawOver(c, parent, state);
+	}
+
+	@Override public void getItemOffsets(Rect outRect, View view, RecyclerView parent,
+		RecyclerView.State state) {
+		super.getItemOffsets(outRect, view, parent, state);
+
+	}
+}
